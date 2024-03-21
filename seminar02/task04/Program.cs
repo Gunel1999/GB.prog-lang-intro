655fe120c5c49d0cdb99609f0cf21064de268990
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 18:40:08 +0400

    Seminar02 task04 added

---
+++ seminar02/task04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = number * (-1);
+}
+
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine($"Третяя цифра числа {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
