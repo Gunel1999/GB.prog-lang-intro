d823c3b1b25b54d10f957af6339655fe2d5822f3
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 18:06:39 +0400

    Seminar02 task03 improved

--- seminar02/task03/Program.cs
+++ seminar02/task03/Program.cs
@@ -3,11 +3,13 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 % number2 == 0)
+int result = number1 % number2;
+
+if (result == 0)
 {
     Console.WriteLine("Кратно");
 }
 else
 {
-    Console.WriteLine($"Не кратно, остаток {number1 % number2}");
+    Console.WriteLine($"Не кратно, остаток {result}");
 }
