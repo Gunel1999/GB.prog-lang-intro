928c054335d1b5b871871abdbc67740f588031e6
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 15:21:56 +0400

    Seminar01 task01 added on initial state

--- seminar01/task01/Program.cs
+++ seminar01/task01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = 2;
+int num2 = 10;
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+} else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
