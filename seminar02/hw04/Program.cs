db7d3d10d2d43dac960e4454a409e46611637769
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 19:01:28 +0400

    Seminar02 hw04 added

---
+++ seminar02/hw04/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = "";
+while (number > 0)
+{
+    result = number % 10 + ", " + result;
+    number = number / 10;
+}
+Console.WriteLine(result);
