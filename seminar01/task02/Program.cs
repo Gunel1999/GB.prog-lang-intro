fa822339ec6c3eb98988f7968e256239f11ef7fe
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 15:51:43 +0400

    Seminar01 task02 added

---
+++ seminar01/task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое положительное число N:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = -num;
+while (count <= num)
+{
+    Console.Write(count + " ");
+    count++;
+}
