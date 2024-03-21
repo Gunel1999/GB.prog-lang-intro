cec2345c83b22bb4fd6ee18f66e4f16603707d4c
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 14:40:23 +0400

    Lesson01 and Seminar01 added

---
+++ lesson01/hw01/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 4;
+int b = 2;
+int c = 6;
+int d = 8;
+
+double product = a * b;
+Console.WriteLine(product);
+double sum = c + d;
+Console.WriteLine(sum);
+
+double division = product / sum;
+Console.WriteLine(division);
+
---
+++ lesson01/task04/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 8;
+int c = 4;
+int d = 2;
+int e = 10;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.Write(max);
