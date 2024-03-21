6975f29c9a40cadffdc90614948e5a9d1213f7cd
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 16:13:49 +0400

    Seminar01 task03 added

---
+++ seminar01/task03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100 && number < 999)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int result = firstDigit + lastDigit;
+    Console.WriteLine($"Сумма первой и последней цифр числа {number} равна {result}");
+}
+else
+{
+    Console.WriteLine("Введено неверное значение");
+}
