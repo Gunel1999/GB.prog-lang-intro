e11f340ccc3fb2c23aa1183ca563fabbe2030ca8
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-21 17:14:06 +0400

    Seminar02 task01 added

---
+++ seminar02/task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000 || number < -99 && number > -1000)
+{
+    int firstDigit = number / 100 * 10;
+    int lastDigit = number % 10;
+    int result = firstDigit + lastDigit;
+    Console.WriteLine($"Сумма первой и последней цифры трехзначного числа {number} равна {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
