d258846324c38dee28ce27124d9f7d3cda576e4c
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 18:23:20 +0400

    Seminar03 task02 added

--- seminar03/task02/Program.cs
+++ seminar03/task02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив из 10 элементов, заполненный числами из промежутка [-10, 10]. Замените отрицательные элементы на положительные, а положительные на отрицательные.
+
+int[] array = new int[10];
+Console.WriteLine("Введите 10 элементов массива от -10 до 10");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i + 1} элемент массива ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write(array[i] + " ");
+}
+
