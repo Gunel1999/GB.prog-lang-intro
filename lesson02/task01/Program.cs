3012474f73803a4513c156fefbb9e2e3165eb994
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 13:02:34 +0400

    Lesson02 task01 added

---
+++ lesson02/task01/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = new int[5];
+arr[0] = 8;
+arr[1] = 2;
+arr[2] = 6;
+arr[3] = 1;
+arr[4] = 3;
+
+int[] arr2 = new int[5] { 3, 4, 1, 2, 5 };
+int[] arr3 = { 8, 4, 3, 2, 6 };
+
+//How to print a full array like an object through commas?
+Console.WriteLine(string.Join(",", arr));
+Console.WriteLine(string.Join(", ", arr2));
+Console.WriteLine(string.Join(", ", arr3));
