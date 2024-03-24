e841c5a892146336e0829b9acfd6427ae7eb3dbc
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 14:36:16 +0400

    Lesson02 task04 added

---
+++ lesson02/task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// int n = 5;
+int[] array = { 2, 3, 6, 9, 10 };
+int count = 0;
+int max = array[0];
+
+while (count < array.Length)
+{
+    if (array[count] > max)
+        max = array[count];
+    count++;
+}
+
+Console.WriteLine(max);
