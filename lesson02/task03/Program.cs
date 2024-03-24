de31834a9e0a313fd9c1ff9745aa0a026e785c1a
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 14:19:25 +0400

    Lesson02 task03 added

---
+++ lesson02/task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18, 10 };
+int count = 0;
+
+while (count < array.Length)
+{
+    if (array[count] % 2 == 0)
+    {
+        Console.Write(array[count] + " ");
+    }
+    count++;
+}
