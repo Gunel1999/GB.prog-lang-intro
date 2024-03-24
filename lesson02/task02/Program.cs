e86d38cd4f3201be494235391591c7a6f90312b1
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 14:00:06 +0400

    Lesson02 task02 added

---
+++ lesson02/task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+int count = 0;
+
+while (count < array.Length)
+{
+    array[count] = count + 1;
+    Console.Write(array[count] + " ");
+    count++;
+}
