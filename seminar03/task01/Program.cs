55d73d554449a5e622ad4f2366bdf863c2d92331
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-03-24 17:59:06 +0400

    Seminar03 task01 added

---
+++ seminar03/task01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве. Программа должна выдать ответ: Да/Нет.
+
+// int[] array = { 1, 3, 4, 19, 3 };
+int[] array = { -4, 3, 4, 1 };
+int find = 3;
+bool result = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == find)
+    {
+        result = true;
+        break;
+    }
+}
+
+Console.WriteLine(result ? "Да" : "Нет");
