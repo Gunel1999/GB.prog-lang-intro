78c80d93e593c53227a795b293abb2e1fb9bcd53
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-04-12 23:16:40 +0400

    Final HomeWork added

---
+++ HW_final/task01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+//Пример: 
+//M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+//M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void PrintNumbers(int m, int n)
+{
+    if (m > n) return;
+    if (m == n) Console.Write(m);
+    else Console.Write(m + ", ");
+    PrintNumbers(m + 1, n);
+}
+
+PrintNumbers(1, 5);
+Console.WriteLine();
+PrintNumbers(4, 8);
---
+++ HW_final/task02/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+
+int m = 2;
+int n = 4;
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine(Akkerman(m, n));
