6d3bca22ae32d19741b3654f679a2a33e6b1ae6e
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-04-12 22:39:40 +0400

    Lesson05 task01 added

---
+++ lesson05/task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вычислить факториал числа N
+int n = 4;
+
+int Factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine("Факториал равен 1");
+        return 1;
+    }
+    else
+    {
+        Console.WriteLine($"Факториал равен {n} * {n - 1}");
+        return n * Factorial(n - 1);
+    }
+}
+
+Console.WriteLine(Factorial(n));
