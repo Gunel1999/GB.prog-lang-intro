f115056002bad22a8358004d6bb6d940e58c5fcb
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-04-15 00:41:14 +0400

    Seminar07 added

---
+++ seminar07/task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumOfDigits(num / 10);
+}
+
+int sum = SumOfDigits(number);
+Console.WriteLine($"Сумма цифр числа {number}");
+Console.WriteLine(sum);
---
+++ seminar07/task03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите слово: ");
+string word = Console.ReadLine();
+
+void PrintConstants(string word, int count)
+{
+    if (count == word.Length) return;
+    string vowels = "aeiouy";
+    if (char.IsAsciiLetter(word[count]) && !vowels.Contains(char.ToLower(word[count])))
+    {
+        Console.Write($"{word[count]} ");
+    }
+    PrintConstants(word, ++count);
+}
+
+PrintConstants(word, 0);
